40ecbffc53d0bb13f6a88ed868d5ca82bccc4ff2
Al <107307734+al-the@users.noreply.github.com>
2023-03-02 13:16:02 +0800

    Update server.js
    latest AI model: gpt-3.5-turbo

--- server/server.js
+++ server/server.js
@@ -26,10 +26,10 @@ app.post('/', async(req,res) => {
         const prompt = req.body.prompt;
 
         const response = await openai.createCompletion({
-            model: "text-curie-001",
+            model: "gpt-3.5-turbo",
             prompt: `${prompt}`, 
             temperature: 0.5, 
-            max_tokens: 2000, 
+            max_tokens: 3000, 
             top_p: 1, 
             frequency_penalty: 0.5, 
             presence_penalty: 0,
