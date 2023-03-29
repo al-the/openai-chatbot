973b76cec81f0f95346d0051e02fea0aac27750a
Al <107307734+al-the@users.noreply.github.com>
2023-03-30 01:58:17 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.post('/', async(req,res) => {
     try{
         const prompt = req.body.prompt;
 
-        const response = await openai.createCompletion.create({
+        const response = await openai.createCompletion({
             model: "text-curie-001",
             prompt: `${prompt}`, 
             temperature: 0.5, 
